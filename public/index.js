6cd00321339b6c7fec1c110c93e1b8c10f4adf9a
James Mendoza <mendozajames02@hotmail.com>
2022-04-01 18:37:56 -0500

    Add Effecto accordion in About section

--- public/index.js
+++ public/index.js
@@ -2,7 +2,6 @@ const menu = document.getElementById('menu')
 const menuOpen = document.getElementById('menu-icon-open')
 const menuClose = document.getElementById('menu-icon-close')
 const links = document.querySelectorAll('#menu a')
-const banner = document.getElementById('banner-id')
 
 
 menuOpen.addEventListener('click', () => {
@@ -18,3 +17,22 @@ links.forEach(link => {
     menu.classList.toggle('active')
   })
 })
+
+const accordionItems = document.querySelectorAll('.accordion .accordion-item')
+
+accordionItems.forEach(item => {
+  let button = item.querySelector('button')
+  let text = item.querySelector('.answer')
+  button.addEventListener('click', () => {
+    deactiveAccordionItem()
+    text.classList.toggle('active')
+  })
+})
+
+function deactiveAccordionItem() {
+  accordionItems.forEach(itemText => {
+    let tagText = itemText.querySelector('.answer')
+    tagText.classList.remove('active')
+  })
+}
+
