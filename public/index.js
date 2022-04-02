3bb19f5f47ab751dfe1045a723d22455af9e1d33
James Mendoza <mendozajames02@hotmail.com>
2022-04-02 14:52:55 -0500

    Add logic to contact section

--- public/index.js
+++ public/index.js
@@ -37,7 +37,30 @@ function deactiveAccordionItem() {
     tagText.classList.remove('active')
     button.classList.remove('active')
   })
+}
 
+const $form = document.getElementById("contact-form-id")
 
+$form.addEventListener("submit", handleSubmit)
+
+async function handleSubmit(event) {
+  event.preventDefault()
+
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+
+  if(response.ok) {
+    this.reset()
+    alert('¡Gracias por contactarnos!, En breve nos comunicaremos para responder su consulta.')
+  } else {
+    this.reset()
+    alert('¡Hubo un problema al enviar el formulario!')
+  }
 }
 
